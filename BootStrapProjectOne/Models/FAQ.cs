1b858809e0bbebe7c9a25bec7a3ee16c67c963b8
Emma <emmahlowe@gmail.com>
2019-11-16 14:40:13 -0700

    Added Models

---
+++ BootStrapProjectOne/Models/FAQ.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BootStrapProjectOne.Models
+{
+    public class FAQ
+    {
+        [Key]
+        public int STUD_FAQ_ID { get; set; }
+
+        public int Q_ID { get; set; }
+    }
+}
