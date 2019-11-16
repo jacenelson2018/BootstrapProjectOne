71d39b2761577554a4575258b00149dc11c39790
jacenelson2018 <jacenelson15@gmail.com>
2019-11-16 14:06:28 -0700

    major pages added

---
+++ BootStrapProjectOne/Controllers/AccController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootStrapProjectOne.Controllers
+{
+    public class AccController : Controller
+    {
+        // GET: Acc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BootStrapProjectOne/Controllers/ISController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootStrapProjectOne.Controllers
+{
+    public class ISController : Controller
+    {
+        // GET: IS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
